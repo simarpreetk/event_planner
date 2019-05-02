3e63801fae3031954c3e73c2c1b46ae66afbde14
simarpreetc <simarpreetchhoker22@gmail.com>
2019-05-01 20:42:07 -0400

    Firebase,notification,cloud functions/triggers

--- src/components/dashboard/notifications.js
+++ src/components/dashboard/notifications.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
-const Notifications =()=>{
+const Notifications =(props)=>{
 
        return(
-        <div>
-            <p> Notifications </p>
+        <div className = "section">
+        <div className = "card z-depth-0">
+        <div className = "card-content">
+        <span className = "card-title">Notifications</span>
+        <ul className = "notifications">
+              <li>Notifications</li>
+              <li>Notifications</li>
+              <li>Notifications</li>
+              <li>Notifications</li>
+              <li>Notifications</li> 
+        </ul>
+        </div>
+        </div>
         </div>
        )
 }
